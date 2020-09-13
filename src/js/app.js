0314d53cfb3ddbd056e04cfe62b0adf6be875c9f
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-09-13 23:00:18 +0300

    function getRandom is modified

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,16 @@
 // получаем поле
 const cells = document.getElementsByClassName('field_cell');
 // получаем рандомный индекс
-export default function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+export default function getRandom(min, max, num) {
+  const rtn = Math.floor(Math.random() * (max - min + 1) + min);
+  return rtn === num ? getRandom(min, max, num) : rtn;
 }
 // const img = './goblin.png'
 let index = getRandom(0, (cells.length - 1));
 // устанавливаем гоблина в первоначальное положение
 // cells[index].firstChild.src = img;
 // имя картинки взято из dist
-cells[index].firstChild.setAttribute('src', 'https://github.com/netology-code/ahj-homeworks/raw/master/dom/pic/goblin.png');
+cells[index].firstChild.setAttribute('src', './img/goblin.png');
 // добавляем значение для активной ячйки с гоблином
 cells[index].classList.add('active');
 // функция для смены поля с гоблином
@@ -18,9 +19,9 @@ function replaceGoblin() {
   // goblin[0].firstChild.src = '';
   goblin[0].firstChild.removeAttribute('src');
   goblin[0].classList.remove('active');
-  index = getRandom(0, (cells.length - 1));
+  index = getRandom(0, (cells.length - 1), index);
   // cells[index].firstChild.src = img;
-  cells[index].firstChild.setAttribute('src', 'https://github.com/netology-code/ahj-homeworks/raw/master/dom/pic/goblin.png');
+  cells[index].firstChild.setAttribute('src', './img/goblin.png');
   cells[index].classList.add('active');
 }
 
